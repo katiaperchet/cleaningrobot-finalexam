d6492bbb4a42f75bb5589a7b253d08095dfb55af
Katia Perchet <53318951+katiaperchet@users.noreply.github.com>
2025-01-28 00:46:34 +0100

    Red phase User Story #2 - >10% - led off

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -34,3 +34,13 @@ class TestCleaningRobot(TestCase):
         robot.pos_y = 1
         robot.heading = robot.S
         self.assertEqual("(1,1,S)", robot.robot_status())
+
+    @patch.object(GPIO, "output")
+    @patch.object(IBS, "get_charge_left")
+    def test_charge_left_more_10_led_off(self, mock_battery: Mock, mock_led: Mock):
+        mock_battery.return_value = 11
+        robot = CleaningRobot()
+        robot.manage_cleaning_system()
+        calls = [((robot.RECHARGE_LED_PIN, False),)]
+        mock_led.assert_has_calls(calls)
+        self.assertFalse(robot.recharge_led_on)
