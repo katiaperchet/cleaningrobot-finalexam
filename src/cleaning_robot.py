648fd86765bf0355ab097106195dd66568ebb5e0
Katia Perchet <53318951+katiaperchet@users.noreply.github.com>
2025-01-28 00:48:36 +0100

    Green phase User Story #2 - >10% - cleaning system on

--- src/cleaning_robot.py
+++ src/cleaning_robot.py
@@ -83,6 +83,8 @@ class CleaningRobot:
     def manage_cleaning_system(self) -> None:
         GPIO.output(self.RECHARGE_LED_PIN, GPIO.LOW)
         self.recharge_led_on = False
+        GPIO.output(self.CLEANING_SYSTEM_PIN, GPIO.HIGH)
+        self.cleaning_system_on = True
 
     def activate_wheel_motor(self) -> None:
         """
